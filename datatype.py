6bc252734816c372a310312c58a56457ca0afb7c
d_puwar_08 <242736805+dpuwar08@users.noreply.github.com>
2026-06-09 12:54:09 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,11 @@
+Name = "Divyaraj Puwar" #data type = str
+Age = 17 #data type - int
+Num = 8.12 #data type - float
+bool = True #data type - boolean
+a = None #data type - None
+
+print ( "Data type of Name: " , type (Name) , "Value : " , Name )
+print ( "Data type of Age: " , type (Age) , "Value : " , Age )
+print ( "Data type of Num: " , type (Num) , "Value : " , Num )
+print ( "Data type of bool: " , type (bool) , "Value : " , bool )
+print ( "Data type of a: " , type (a) , "Value : " , a )

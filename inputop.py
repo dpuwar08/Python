44c092ae13501befe29a1e82727360d0f601620c
d_puwar_08 <242736805+dpuwar08@users.noreply.github.com>
2026-06-10 18:40:04 +0530

    Add files via upload

---
+++ inputop.py
@@ -0,0 +1,16 @@
+a = int(input ("Enter a Value: "))
+b = int(input ("Enter another value: "))
+
+sum = a + b
+diff = a - b
+multi = a * b
+div = a / b
+mod = a % b
+pow = a ** b
+
+print("Sum of a and b: " , sum)
+print("Differecne of a and b: " , diff)
+print("Multiplication of a and b: " , multi)
+print("division of a by b: " , div)
+print("remainder of a modulo b: " , mod)
+print("Value of a raise to b: " , pow)

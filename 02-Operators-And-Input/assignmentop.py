01af8cf7cd098025ffcc993a814184cab733f1bc
d_puwar_08 <242736805+dpuwar08@users.noreply.github.com>
2026-06-10 18:03:53 +0530

    Add files via upload

---
+++ 02-Operators-And-Input/assignmentop.py
@@ -0,0 +1,21 @@
+num = 5
+sum = 5
+diff = 5
+multi = 5
+div = 5
+mod = 5
+pow = 2
+
+sum += num # Means - sum = sum + num
+diff -=num # Means - diff = diff - num
+multi *= num # Means - multi = multi * num
+div /= num # Means - div = div / num
+mod %= num # Means - mod = mod % num
+pow **= num # Means - pow = pow ** num
+
+print (sum) # 10
+print (diff) # 0
+print (multi) # 25
+print (div) # 1
+print (mod) # 0
+print (pow) # 32

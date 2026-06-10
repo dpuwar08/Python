5723c983f5eb841741cc999a065573e5abe0df15
d_puwar_08 <242736805+dpuwar08@users.noreply.github.com>
2026-06-10 17:55:42 +0530

    Add files via upload

---
+++ 02-Operators-And-Input/relationalop.py
@@ -0,0 +1,9 @@
+a = 5
+b = 10
+
+print( a == b ) #False - return 0
+print( a != b ) #True - return 1
+print( a > b ) #False - return 0
+print( a < b ) #True - return 1
+print( a >= b ) #False - return 0
+print( a <= b ) #True - return 1

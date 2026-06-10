e09be4d08ebf18b8af43283ac28f32c8503bdb25
d_puwar_08 <242736805+dpuwar08@users.noreply.github.com>
2026-06-10 18:45:22 +0530

    Add files via upload

---
+++ 02-Operators-And-Input/rectanglearea.py
@@ -0,0 +1,6 @@
+length = float(input("Enter the length of Square: "))
+breadth =  float(input("Enter the breadth of Square: "))
+
+area = length * breadth
+
+print ("The are of Rectangle: " , area)
---
+++ 02-Operators-And-Input/squarearea.py
@@ -0,0 +1,5 @@
+length = float(input("Enter the length of Square: "))
+
+area = length * length
+
+print ("The are of Square: " , area)

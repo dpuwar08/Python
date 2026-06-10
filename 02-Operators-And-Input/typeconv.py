a20c0a7ce51e96ecaa45677d53526a7ee7f3c83a
d_puwar_08 <242736805+dpuwar08@users.noreply.github.com>
2026-06-10 18:21:48 +0530

    Add files via upload

---
+++ 02-Operators-And-Input/typeconv.py
@@ -0,0 +1,17 @@
+#Type conversion :-
+
+a = 2
+b = 4.25
+
+sum = a + b #2.0(a - int to float) + 4.25
+
+print (sum)
+
+#Type casting :-
+
+a = int( "4" )
+b = 2.25
+
+sum = a + b #4.0(a - str to float) + 2
+
+print(sum)

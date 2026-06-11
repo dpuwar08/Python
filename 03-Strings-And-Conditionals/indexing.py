2d4975625c77260686a43424f344bcb8001c852d
d_puwar_08 <242736805+dpuwar08@users.noreply.github.com>
2026-06-11 19:20:35 +0530

    Add files via upload

---
+++ 03-Strings-And-Conditionals/indexing.py
@@ -0,0 +1,41 @@
+name = "Divyarajsinh Puwar" 
+
+#This will print one character in one line!
+
+ch0 = name[0]
+ch1 = name[1]
+ch2 = name[2]
+ch3 = name[3]
+ch4 = name[4]
+ch5 = name[5]
+ch6 = name[6]
+ch7 = name[7]
+ch8 = name[8]
+ch9 = name[9]
+ch10 = name[10]
+ch11 = name[11]
+ch12 = name[12]
+ch13 = name[13]
+ch14 = name[14]
+ch15 = name[15]
+ch16 = name[16]
+ch17 = name[17]
+
+print(ch0)
+print(ch1)
+print(ch2)
+print(ch3)
+print(ch4)
+print(ch5)
+print(ch6)
+print(ch7)
+print(ch8)
+print(ch9)
+print(ch10)
+print(ch11)
+print(ch12)
+print(ch13)
+print(ch14)
+print(ch15)
+print(ch16)
+print(ch17)
